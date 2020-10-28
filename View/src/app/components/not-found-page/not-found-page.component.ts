bce2351a10e8240f942d49f47a47ae2f95c1eeda
NikitaShtytko <shtytko.nikita@gmail.com>
2020-10-28 22:54:37 +0300

    add 404 difference pages

--- View/src/app/components/not-found-page/not-found-page.component.ts
+++ View/src/app/components/not-found-page/not-found-page.component.ts
@@ -9,7 +9,15 @@ export class NotFoundPageComponent implements OnInit {
 
   constructor() { }
 
+  public random;
+
   ngOnInit(): void {
+    this.getRandomInt(4);
+  }
+
+    getRandomInt(max) {
+    this.random =  Math.floor(Math.random() * Math.floor(max)) + 1;
+    console.log(this.random);
   }
 
 }
