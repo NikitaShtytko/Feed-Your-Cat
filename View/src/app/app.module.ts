991f4f560cf899aaa52d3503c15495f98a7eafd5
NikitaShtytko <shtytko.nikita@gmail.com>
2020-10-28 22:21:14 +0300

    register and login pages

--- View/src/app/app.module.ts
+++ View/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import {RouterModule, Routes} from "@angular/router";
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+const appRoutes: Routes =[
+  { path: '', component: LoginPageComponent},
+  { path: 'login', component: LoginPageComponent},
+  { path: 'register', component: RegisterPageComponent},
+  { path: '404', component: NotFoundPageComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    NotFoundPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    MDBBootstrapModule.forRoot(),
+    NoopAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
