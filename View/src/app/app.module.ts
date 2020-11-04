f1a3c2309ce8f5b8649ff271c1b740dea92001a2
NikitaShtytko <shtytko.nikita@gmail.com>
2020-11-04 23:18:50 +0300

    new features for user

--- View/src/app/app.module.ts
+++ View/src/app/app.module.ts
@@ -1,30 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginPageComponent } from './components/login-page/login-page.component';
-import { RegisterPageComponent } from './components/register-page/register-page.component';
-import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginPageComponent} from './components/login-page/login-page.component';
+import {RegisterPageComponent} from './components/register-page/register-page.component';
+import {NotFoundPageComponent} from './components/not-found-page/not-found-page.component';
 import {RouterModule, Routes} from "@angular/router";
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { UsersListComponent } from './components/admin/users-list/users-list.component';
-import { FeedersListComponent } from './components/admin/feeders-list/feeders-list.component';
-import { RequestsListComponent } from './components/admin/requests-list/requests-list.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {UsersListComponent} from './components/admin/users-list/users-list.component';
+import {FeedersListComponent} from './components/admin/feeders-list/feeders-list.component';
+import {RequestsListComponent} from './components/admin/requests-list/requests-list.component';
 import {AdminPanelComponent} from "./components/admin/admin-panel/admin-panel.component";
-import { FeedersPageComponent } from './components/user/feeders-page/feeders-page.component';
+import {FeedersPageComponent} from './components/user/feeders-page/feeders-page.component';
 
-const appRoutes: Routes =[
-  { path: '', component: LoginPageComponent},
-  { path: 'login', component: LoginPageComponent},
-  { path: 'register', component: RegisterPageComponent},
-  { path: 'admin', component: AdminPanelComponent},
-  { path: 'admin/users', component: UsersListComponent},
-  { path: 'admin/feeders', component: FeedersListComponent},
-  { path: 'admin/requests', component: RequestsListComponent},
+const appRoutes: Routes = [
+  {path: 'login', component: LoginPageComponent},
+  {path: 'register', component: RegisterPageComponent},
+  {path: 'admin', component: AdminPanelComponent},
+  {path: 'admin/users', component: UsersListComponent},
+  {path: 'admin/feeders', component: FeedersListComponent},
+  {path: 'admin/requests', component: RequestsListComponent},
+  {path: '', component: FeedersPageComponent},
 
-  { path: '**', component: NotFoundPageComponent},
+  {path: '**', component: NotFoundPageComponent},
 ];
 
 @NgModule({
@@ -49,4 +49,5 @@ const appRoutes: Routes =[
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
