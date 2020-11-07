f1bf4876726b5351e7f1943b4a78273dac1152fb
NikitaShtytko <shtytko.nikita@gmail.com>
2020-11-07 23:58:16 +0300

    init modal window

--- View/src/app/components/user/feeders-page/feeders-page.component.ts
+++ View/src/app/components/user/feeders-page/feeders-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
+import {ConfirmDialogComponent} from "../../admin/confirm-dialog/confirm-dialog.component";
+import {FeederModalComponent} from "../feeder-modal/feeder-modal.component";
 
 @Component({
   selector: 'app-feeders-page',
@@ -11,10 +14,30 @@ export class FeedersPageComponent implements OnInit {
   // public arr = [1,2,3,4,5,6,7,8, 9, 10, 11, 12, 13, 14, 15, 16];
   public arr = [1,2,3,4,5,6,7,8, 9, 10, 11, 12, 13, 14];
 
-  constructor() { }
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit(): void {
 
+  }
+
+  openDialog(way) {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+
+    dialogConfig.data = {
+      title: way
+    };
+    const dialogRef = this.dialog.open(FeederModalComponent, dialogConfig);
+
+    if (dialogConfig){
+
+    }
+
+    dialogRef.afterClosed().subscribe(
+      data => console.log("Dialog output:", data)
+    );
   }
 
 }
