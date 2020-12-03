fa6b486c5ceb1d24a006d2baa69b2fd1c40ce2a4
aratorr <aratorr39@gmail.com>
2020-12-03 13:02:33 +0300

    try controller

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FeedYourCat.Models
+{
+    public class User : IdentityUser
+    { 
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
