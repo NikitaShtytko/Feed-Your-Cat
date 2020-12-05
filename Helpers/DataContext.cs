edf118e7c39e646b856c3f442a3ac9f02e4c6c69
aratorr <aratorr39@gmail.com>
2020-12-05 12:06:15 +0300

    implement registration and CRUD

---
+++ Helpers/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using FeedYourCat.Entities;
+
+namespace FeedYourCat.Helpers
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server database
+            options.UseSqlServer("");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
